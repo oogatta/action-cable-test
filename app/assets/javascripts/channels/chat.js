df54dd94a4a2023ff054bcfb0e73a5ac3474665a
Naohiro Ohgata <oogatta@gmail.com>
2019-12-19 14:25:40 +0900

    styles

--- app/assets/javascripts/channels/chat.js
+++ app/assets/javascripts/channels/chat.js
@@ -1,9 +1,11 @@
 $(document).on('turbolinks:load', () => {
-    if ($('body').hasClass('messages')) {
-        App.chat = App.cable.subscriptions.create('ChatChannel', {
-            received: (data) => {
-                console.log(data);
-            }
-        });
+  if ( !$('body').hasClass('messages') ) {
+    return;
+  }
+
+  App.chat = App.cable.subscriptions.create('ChatChannel', {
+    received: (data) => {
+      console.log(data);
     }
+  });
 });
--- app/assets/javascripts/channels/community.js
+++ app/assets/javascripts/channels/community.js
@@ -1,9 +1,11 @@
 $(document).on('turbolinks:load', () => {
-    if ($('body').hasClass('communities')) {
-        App.community = App.cable.subscriptions.create({channel: "CommunityChannel", community_id: "1"}, {
-            received: (data) => {
-                console.log(data);
-            }
-        });
+  if ( !$('body').hasClass('communities') ) {
+    return;
+  }
+
+  App.community = App.cable.subscriptions.create({ channel: "CommunityChannel", community_id: "1" }, {
+    received: (data) => {
+      console.log(data);
     }
+  });
 });
